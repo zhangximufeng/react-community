fe067f88ea1c9319ba8bfd3f3c9a98fdf0e3f924
张熙沐枫 <zhangximufeng@gmail.com>
2018-09-05 16:50:23 +0800

    test precommit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const fs = require('fs')
+const fs = require("fs")
 const path = require('path')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
